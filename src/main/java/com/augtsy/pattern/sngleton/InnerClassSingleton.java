94a54969a3ecad5b8197a505d72382aeabd8d9c5
aug_tsy <18761617801@163.com>
2022-05-30 14:26:56 +0800

    内部类单例

--- src/main/java/com/augtsy/pattern/sngleton/InnerClassSingleton.java
+++ src/main/java/com/augtsy/pattern/sngleton/InnerClassSingleton.java
@@ -2,8 +2,8 @@ package com.augtsy.pattern.sngleton;
 
 /**
  * @Description: 静态内部类单例模式
- * 第一次加载StaticClassSingleton类时，并不会实例化instance，
- * 只有第一次调用getInstance方法时，Java虚拟机才会去加载StaticClassSingletonHolder类，
+ * 第一次加载InnerClassSingleton类时，并不会实例化instance，
+ * 只有第一次调用getInstance方法时，Java虚拟机才会去加载SingletonHolder类，
  * 继而实例化instance，这样延时实例化instance，节省了内存，并且也是线程安全的。这是推荐使用的一种单例模式。
  * @Auther: tsy
  * @Date: 2022/05/24/3:18 下午
